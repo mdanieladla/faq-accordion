a7a8ec7bda2d6109af022c08981e3a1b149d5694
mdanieladla <mariadanieladarnea@gmail.com>
2021-11-17 13:29:51 +0100

    tried collapsable

--- main.js
+++ main.js
@@ -2,16 +2,21 @@
 
 let contentToggle = document.querySelectorAll('.js-toggle');
 
-function toggleParagraph() {
-  let itemClass = this.parentNode.className; //itemClass hace referencia a donde hacemos click, en este caso es: skills__content skills__open js-skills.
-  for (let index = 0; index < contentToggle.length; index++) {
-    contentToggle[index].className = 'skills__content skills__close js-skills';
-  }
-  if (itemClass === 'skills__content skills__close js-skills') {
-    this.parentNode.className = 'skills__content skills__open js-skills';
+const iconRotate = document.querySelectorAll('.js-icon');
+
+function handleClick(ev) {
+  let clicked = ev.currentTarget;
+  const click = clicked.parentNode;
+  for (let index = 0; index < iconRotate.length; index++) {
+    if (clicked === iconRotate[index]) {
+      contentToggle[index].classList.toggle('hidden');
+      iconRotate[index].classList.toggle('icon-end');
+    } else {
+      contentToggle[index].classList.add('hidden');
+    }
   }
 }
 
-pToggle.forEach((elemt) => {
-  elemt.addEventListener('click', toggleParagraph);
-});
+for (const item of iconRotate) {
+  item.addEventListener('click', handleClick);
+}
