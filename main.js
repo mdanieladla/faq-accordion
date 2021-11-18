0e0d6998fa97d70e415754112e558c810bb06ba0
mdanieladla <mariadanieladarnea@gmail.com>
2021-11-18 16:50:17 +0100

    finished project

--- main.js
+++ main.js
@@ -1,26 +1,23 @@
 'use strict';
-
+//traemos elementos de los bloques que contienen la pregunta y respuesta.
 const questionBlock = document.querySelectorAll('.js-qb');
 
-//console.log(questionBlock);
-
+//traemos elementos de los iconos.
 const iconRotate = document.querySelectorAll('.js-icon');
-//console.log(iconRotate);
+
+//traemos elementos de las preguntas.
+const questions = document.querySelectorAll('.js-q');
 
 //cuando haga click en el bloque de preguntas, si la respuesta tiene la clase hidden se la quite y si no la tiene se la añada, a su vez rote el icono.
 
 function handleClick(ev) {
-  let clicked = ev.currentTarget;
-  //console.log(clicked);
-  const click = clicked.parentNode;
-  console.log(click);
+  let clicked = ev.currentTarget; //donde el usuario hace click que en este caso es el icono.
+  const click = clicked.parentNode; //el contenedor padre del icono, el párrafo.
   for (let index = 0; index < iconRotate.length; index++) {
     if (clicked === iconRotate[index]) {
       questionBlock[index].classList.toggle('hidden');
       iconRotate[index].classList.toggle('icon-end');
-    } else {
-      iconRotate[index].classList.toggle('icon-end');
-      //console.log(iconRotate[index]);
+      questions[index].classList.toggle('bold');
     }
   }
 }
